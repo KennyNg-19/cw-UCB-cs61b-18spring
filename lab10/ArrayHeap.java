022a476d090daf2c23c83c1a76fb3a0e6fb2369a
Kenny Ng <wuyuhao2019@126.com>
2019-10-01 17:40:25 +0800

    MinPQ: tree ADT, swim/bubble方法和sink方法不一样，前者是向上比，node和唯一的parent比较，而sink是向下，因为binary所以会有俩node，parent得和left right node中min的swap，多了一次取min

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * A Generic heap class. Unlike Java's priority queue, this heap doesn't just
@@ -8,27 +9,32 @@ import static org.junit.Assert.*;
  * will be useful later on in the class...
  */
 public class ArrayHeap<T> implements ExtrinsicPQ<T> {
-    private Node[] contents;
-    private int size;
+
+    //toString() 生成的heap树状图，横着看，别竖着看！
+
+    /* 因为是array, 如果加入node不用insert而使用array assignment，是按照加入顺序的！*/
+    private Node[] contents; // node,模拟tree结构，实际是array的index实现tree
 
     public ArrayHeap() {
+        /* ArrayHeap.Node是什么？*/
         contents = new ArrayHeap.Node[16];
 
         /* Add a dummy item at the front of the ArrayHeap so that the getLeft,
          * getRight, and parent methods are nicer. */
-        contents[0] = null;
+        contents[0] = null; // 从1开始
 
         /* Even though there is an empty spot at the front, we still consider
          * the size to be 0 since nothing has been inserted yet. */
         size = 0;
     }
+    private int size;
 
     /**
      * Returns the index of the node to the left of the node at i.
      */
     private static int leftIndex(int i) {
         /* TODO: Your code here! */
-        return 0;
+        return 2 * i;
     }
 
     /**
@@ -36,7 +42,7 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     private static int rightIndex(int i) {
         /* TODO: Your code here! */
-        return 0;
+        return 2 * i + 1;
     }
 
     /**
@@ -44,7 +50,25 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     private static int parentIndex(int i) {
         /* TODO: Your code here! */
-        return 0;
+        return i / 2;
+    }
+
+    /**
+     * Returns the index of the node with smaller value, higher priority.
+     * Precondition: not both nodes are null.
+     */
+    private int min(int index1, int index2) {
+        Node node1 = getNode(index1);
+        Node node2 = getNode(index2);
+        if (node1 == null) {
+            return index2;
+        } else if (node2 == null) {
+            return index1;
+        } else if (node1.myPriority < node2.myPriority) {
+            return index1;
+        } else {
+            return index2;
+        }
     }
 
     /**
@@ -80,46 +104,64 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         contents[index2] = node1;
     }
 
-
     /**
-     * Returns the index of the node with smaller priority. Precondition: not
-     * both nodes are null.
-     */
-    private int min(int index1, int index2) {
-        Node node1 = getNode(index1);
-        Node node2 = getNode(index2);
-        if (node1 == null) {
-            return index2;
-        } else if (node2 == null) {
-            return index1;
-        } else if (node1.myPriority < node2.myPriority) {
-            return index1;
-        } else {
-            return index2;
-        }
-    }
-
-
-    /**
-     * Bubbles up the node currently at the given index.
+     * Bubbles up the node currently at the given index. 直到改到的地方——recursion
      */
     private void swim(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
         /** TODO: Your code here. */
-        return;
+        if (index == 1) return; // recursion别忘了base case
+
+        int parentIndex = parentIndex(index);
+        if (min(index, parentIndex) == index) {
+            swap(index, parentIndex);
+            // recursion
+            swim(parentIndex(index)); // swap后，index做新的parentIndex(index)
+        }
     }
 
+
     /**
-     * Bubbles down the node currently at the given index.
+     * swim/bubble方法和sink方法不一样:
+     * 前者是node和唯一的parent比较，而因为binary的性质，
+     * 后者是parent和left right node中min的swap，多了一次取min
+     * */
+
+    /**
+     * Bubbles down the node 值较大 优先性较低，currently at the given index .
+     * 默认先比较left node
+     *
+     * 但是swim/bubble方法和sink方法不一样，
+     * 前者是node和唯一的parent比较，
+     * 而因为binary的性质，
+     * 后者是parent和left right node中min的swap，多了一次取min
      */
     private void sink(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
         /** TODO: Your code here. */
-        return;
+        // base case: leaf node
+        if (leftIndex(index) > size && rightIndex(index) > size) return;
+
+        /* 默认比较left node*/
+        int smallerIndex = min(leftIndex(index), rightIndex(index));
+        if (min(index, smallerIndex) == smallerIndex) { // 该index的value较大
+            swap(index, smallerIndex);
+            sink(smallerIndex); // 此时index已经换到smallerIndex了
+        }
+    }
+
+    /**
+     * Returns the Node with the smallest priority value, but does not remove it
+     * from the heap. To implement this, return the item in the 1st position of the ArrayList.
+     */
+    @Override
+    public T peek() {
+        /* TODO: Your code here! */
+        return getNode(1).myItem;
     }
 
     /**
@@ -134,31 +176,62 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         }
 
         /* TODO: Your code here! */
-    }
+        /* 用到swim*/
 
-    /**
-     * Returns the Node with the smallest priority value, but does not remove it
-     * from the heap. To implement this, return the item in the 1st position of the ArrayList.
-     */
-    @Override
-    public T peek() {
-        /* TODO: Your code here! */
-        return null;
+        /* 1. 先简单的assignment：放到in the left-most open spot 即 in the bottom level of the tree.*/
+        int index = size + 1;// size总1开始的，所以不用0-1
+        contents[index] = new Node(item, priority);
+        size++;// size得先+1 否则swim里的validateSinkSwimArg会报indexOutOfBound错！
+        /* 2. Swap the item you just added with its parent until it is larger than its parent,
+         * or until it is the new root. */
+        swim(index);
     }
 
     /**
      * Returns the Node with the smallest priority value, and removes it from
-     * the heap. This is dequeue, or poll. To implement this, swap the last
-     * item from the heap into the root position, then sink the root. This is
-     * equivalent to firing the president of the company, taking the last
+     * the heap. This is dequeue, or poll.
+     * To implement this, swap the last item from the heap into the root position,
+     * then sink the root. This is equivalent to firing the president of the company, taking the last
      * person on the list on payroll, making them president, and then demoting
      * them repeatedly. Make sure to avoid loitering by nulling out the dead
      * item.
+     *
+     * Bubble down the new root until it is smaller than both its children.
+     * If you reach a point where you can either bubble down through the left or right child,
+     * you must choose the SMALLER of the two. This process is also called sinking.
      */
     @Override
     public T removeMin() {
         /* TODO: Your code here! */
-        return null;
+        /* 1. Swap the item at the root with the item of the right-most leaf node. */
+        swap(1, size); // size总1开始的，所以不用0-1
+        /* 2. Remove the right-most leaf node, which now contains the min item.*/
+        //抽象实现：赋值为0, size -1?
+        T value = contents[size].item();
+        contents[size] = null; // GC会处理那个node
+        size--;
+        /* 3. Bubble down the new root until it is smaller than BOTH its children.
+        If you reach a point where you can either bubble down through the left or right child,
+        you must choose the smaller of the two——sink里要实现比较了俩children */
+        sink(1);
+
+        return value;
+    }
+
+    /**
+     * 【bonus】
+     * Change the node in this heap with the given item to have the given
+     * priority.
+     * You can assume the heap will not have two nodes with the same
+     * item.
+     * Check item equality with .equals(), not ==. This is a challenging
+     * bonus problem, but shouldn't be too hard if you really understand heaps
+     * and think about the algorithm before you start to code.
+     */
+    @Override
+    public void changePriority(T item, double priority) {
+        /* TODO: Your code here! */
+        return;
     }
 
     /**
@@ -172,24 +245,40 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     }
 
     /**
-     * Change the node in this heap with the given item to have the given
-     * priority. You can assume the heap will not have two nodes with the same
-     * item. Check item equality with .equals(), not ==. This is a challenging
-     * bonus problem, but shouldn't be too hard if you really understand heaps
-     * and think about the algorithm before you start to code.
-     */
-    @Override
-    public void changePriority(T item, double priority) {
-        /* TODO: Your code here! */
-        return;
-    }
-
-    /**
      * Prints out the heap sideways. Provided for you.
+     * 横着看，别竖着看！
      */
     @Override
     public String toString() {
         return toStringHelper(1, "");
+    }
+
+    @Test
+    public void testSwim() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.size = 7;
+        for (int i = 1; i <= 7; i += 1) {
+            // 这里加入的使用array assignment，没有用add所以是按照加入顺序的！
+            pq.contents[i] = new ArrayHeap<String>.Node("x" + i, i);
+        }
+        // Change item x6's priority to a low value.
+
+        pq.contents[6].myPriority = 0;
+        System.out.println("PQ before swimming:");
+        System.out.println(pq);
+
+        // Swim x6 upwards. It should reach the root.
+
+        pq.swim(6);
+        System.out.println("PQ after swimming:");
+        System.out.println(pq);
+        assertEquals("x6", pq.contents[1].myItem);
+        assertEquals("x2", pq.contents[2].myItem);
+        assertEquals("x1", pq.contents[3].myItem);
+        assertEquals("x4", pq.contents[4].myItem);
+        assertEquals("x5", pq.contents[5].myItem);
+        assertEquals("x3", pq.contents[6].myItem);
+        assertEquals("x7", pq.contents[7].myItem);
     }
 
     /* Recursive helper method for toString. */
@@ -229,28 +318,6 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         }
     }
 
-    private class Node {
-        private T myItem;
-        private double myPriority;
-
-        private Node(T item, double priority) {
-            myItem = item;
-            myPriority = priority;
-        }
-
-        public T item(){
-            return myItem;
-        }
-
-        public double priority() {
-            return myPriority;
-        }
-
-        @Override
-        public String toString() {
-            return myItem.toString() + ", " + myPriority;
-        }
-    }
 
 
     /** Helper function to resize the backing array when necessary. */
@@ -275,31 +342,27 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         assertEquals(5, parentIndex(11));
     }
 
-    @Test
-    public void testSwim() {
-        ArrayHeap<String> pq = new ArrayHeap<>();
-        pq.size = 7;
-        for (int i = 1; i <= 7; i += 1) {
-            pq.contents[i] = new ArrayHeap<String>.Node("x" + i, i);
+    private class Node {
+        private T myItem;
+        private double myPriority;
+
+        private Node(T item, double priority) {
+            myItem = item;
+            myPriority = priority;
         }
-        // Change item x6's priority to a low value.
 
-        pq.contents[6].myPriority = 0;
-        System.out.println("PQ before swimming:");
-        System.out.println(pq);
+        public T item() {
+            return myItem;
+        }
 
-        // Swim x6 upwards. It should reach the root.
+        public double priority() {
+            return myPriority;
+        }
 
-        pq.swim(6);
-        System.out.println("PQ after swimming:");
-        System.out.println(pq);
-        assertEquals("x6", pq.contents[1].myItem);
-        assertEquals("x2", pq.contents[2].myItem);
-        assertEquals("x1", pq.contents[3].myItem);
-        assertEquals("x4", pq.contents[4].myItem);
-        assertEquals("x5", pq.contents[5].myItem);
-        assertEquals("x3", pq.contents[6].myItem);
-        assertEquals("x7", pq.contents[7].myItem);
+        @Override
+        public String toString() {
+            return myItem.toString() + ", " + myPriority;
+        }
     }
 
     @Test
